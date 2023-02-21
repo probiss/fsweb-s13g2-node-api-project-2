1feb205a26cf9103cf4619cceaa7dc7e8a39a0b4
probiss <bulut.cihad@gmail.com>
2023-02-21 11:44:42 +0300

    index

--- api/server.js
+++ api/server.js
@@ -1,2 +1,23 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+
+const Posts = require('./posts/posts-model');
+
+const server = express();
+server.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require('./api/server');
+
+const port = 6000;
+
+server.listen(port,()=>{
+    console.log(`Server is working at ${port}`);
+});
